67b10e14033dff6bc3d7586eb214e9f0a0ae6b51
laurensruiz <laurenruiz97@yahoo.com>
2022-05-12 13:30:08 -0400

    fortunes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const fortunes = require('./fortunes')
 const server = express();
 
 server.get('/', (req, res) => {
@@ -8,6 +9,11 @@ server.get('/', (req, res) => {
 
 server.get('/node-env', (req, res) => {
     res.send({NODE_ENV: process.env.NODE_ENV})
+})
+
+server.get('/fortunes', (req, res) => {
+    let randomIndex = Math.floor(Math.random() *fortunes)
+    res.send(`<h1> ${fortunes[randomIndex]} </h1>`)
 })
 
 
